b67feecc30345f70c9cebd3733c637d6daf91df4
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-10 18:13:56 +0000

    uploaded solutions

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,47 +3,75 @@ import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		q1();
-		q2();
-		q3();
-		q4();
-		q5();
-		q6();
 		q7();
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		System.out.println((input.nextLine()).charAt(0));
+		input.close();
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		System.out.println(!(input.nextBoolean()));
+		input.nextLine();
+		input.close();
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		System.out.println((input.nextInt()>5));
+		input.nextLine();
+		input.close();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		int number1 = input.nextInt();
+		input.nextLine();
+		System.out.println((number1 <=2 && number1 >=-2));
+		input.close();
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		System.out.println((input.nextLine()).equals("Hello World"));
+		input.close();
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		int number1 = input.nextInt();
+		input.nextLine();
+		System.out.print("In: ");
+		int number2 = input.nextInt();
+		input.nextLine();
+		System.out.println(number1<=number2);
+		input.close();
 		
 	}
 
 	public static void q7() {
-		//Write question 7 code here
+		System.out.print("In: ");
+		Scanner input = new Scanner(System.in);
+		double number1 = input.nextDouble();
+		input.nextLine();
+		System.out.print("In: ");
+		double number2 = input.nextDouble();
+		input.nextLine();
+		System.out.println(number1>number2);
+		input.close();
 		
 	}
 
